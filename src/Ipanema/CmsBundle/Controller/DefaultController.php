8448679e4ca1b2f1017087ebd72ea04b58af87fa
Nacho Martín <nitram.ohcan@gmail.com>
2013-02-23 21:34:12 +0100

    multilingual pages can be added

--- src/Ipanema/CmsBundle/Controller/DefaultController.php
+++ src/Ipanema/CmsBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Ipanema\CmsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ipanema\CmsBundle\Document\Page;
 use Ipanema\CmsBundle\Document\MultilangPage;
 
 class DefaultController extends Controller
@@ -15,13 +16,39 @@ class DefaultController extends Controller
     {
 
         $dm = $this->get('doctrine_phpcr.odm.document_manager');
-        $page = new MultilangPage();
+        $page = new Page();
         $path = $path.'/newpage';
         $page->setId($path);
         $page->setName('Name');
         $page->setLabel('Label');
         $page->setTitle('Title');
         $page->setBody('Body');
+        $dm->persist($page);
+        $dm->flush();
+        return $this->redirect($this->generateUrl($page));
+    }
+
+    /**
+     * Creates a multilingual page so we can edit it later
+     */
+    public function addMultilingualPageAction($path)
+    {
+
+        $dm = $this->get('doctrine_phpcr.odm.document_manager');
+        $page = new MultilangPage(true);
+        $parent = $dm->find(null, $path);
+        $page->setParent($parent);
+        $page->setName('Name');
+        $dm->persist($page);
+        $page->setLabel('Label-en');
+        $page->setTitle('Title-en');
+        $page->setBody('Body-en');
+        $dm->bindTranslation($page, 'en');
+        $page->setLabel('Label-de');
+        $page->setTitle('Title-de');
+        $page->setBody('Body-de');
+        $page->setLocale('de');
+        $dm->bindTranslation($page, 'de');
         $dm->persist($page);
         $dm->flush();
         return $this->redirect($this->generateUrl($page));
