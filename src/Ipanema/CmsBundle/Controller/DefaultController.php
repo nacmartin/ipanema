901b6be2646f9412b217ce65d8e865a397c9c557
Nacho Martín <nitram.ohcan@gmail.com>
2013-02-23 20:55:22 +0100

    ability to remove pages

--- src/Ipanema/CmsBundle/Controller/DefaultController.php
+++ src/Ipanema/CmsBundle/Controller/DefaultController.php
@@ -3,9 +3,42 @@
 namespace Ipanema\CmsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Ipanema\CmsBundle\Document\MultilangPage;
 
 class DefaultController extends Controller
 {
+
+    /**
+     * Creates an empty page so we can edit it later
+     */
+    public function addPageAction($path)
+    {
+
+        $dm = $this->get('doctrine_phpcr.odm.document_manager');
+        $page = new MultilangPage();
+        $path = $path.'/newpage';
+        $page->setId($path);
+        $page->setName('Name');
+        $page->setLabel('Label');
+        $page->setTitle('Title');
+        $page->setBody('Body');
+        $dm->persist($page);
+        $dm->flush();
+        return $this->redirect($this->generateUrl($page));
+    }
+
+    /**
+     * Removes a page
+     */
+    public function removePageAction($path)
+    {
+
+        $dm = $this->get('doctrine_phpcr.odm.document_manager');
+
+        $page = $dm->find(null, $path);
+        $parent = $page->getParent();
+        $dm->remove($page);
+        $dm->flush();
+        return $this->redirect($this->generateUrl($parent));
+    }
 }
